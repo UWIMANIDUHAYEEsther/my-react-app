83c4c47732b2a5c64894485a9055a3ff863ae366
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-17 07:19:59 +0200

    Added Usergreeting component using ternary operator and updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 
-import Student from'./Student.jsx'
+import Usergreeting  from './Usergreeting.jsx'
 
 function App(){
     return(
         <>
-        <Student name="Ganza" age={40} isstudent={true}/>
-            <Student name="Beza" age={12} isstudent={false}/>
-                <Student name="Tutu" age={23} isstudent={false}/>
-                    <Student name="Hek" age={34} isstudent={true}/>
-                       <student/>
+        <Usergreeting isLoggedIn={false} username='Esther'/>
         
         
         </>

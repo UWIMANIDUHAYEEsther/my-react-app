7836acfd70b6acb32ce15ca81e04c061a95f2c0e
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-17 20:26:19 +0200

    Click event using parameters with SyntheticBaseEvent

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,20 +1,13 @@
 
 function Button() {
-    let count=0
-    const handleClick = (name) => {
-        if (count <3){
-            count ++;
-            console.log(`${name} You clicked me ${count} time/s`)
-        }
-        else{
-            console.log(`${name} stop clicking me `)
-        }
+  const   handleClick=(e)=>e.target.textContent='OUCH! 😖'
+    
 
-    }
+
 
  
     return (
-        <button onClick={()=>handleClick('Esther')}>
+        <button onClick={(e)=>handleClick(e)}>
             Click me 😀
         </button>
     );

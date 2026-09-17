e845503aee93b467ac13da1352cdf652cefbca18
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-17 20:02:14 +0200

    Click event with conditions

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,13 +1,20 @@
 
 function Button() {
-    const handleClick = () => console.log("OUCH!");
+    let count=0
+    const handleClick = (name) => {
+        if (count <3){
+            count ++;
+            console.log(`${name} You clicked me ${count} time/s`)
+        }
+        else{
+            console.log(`${name} stop clicking me `)
+        }
 
-    const handleClick2 = (name) => {
-        console.log(`${name} stop clicking me`);
-    };
+    }
 
+ 
     return (
-        <button onClick={() => handleClick2("Esther")}>
+        <button onClick={()=>handleClick('Esther')}>
             Click me 😀
         </button>
     );

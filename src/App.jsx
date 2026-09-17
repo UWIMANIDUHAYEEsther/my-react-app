3faecf2525ea5675477bbc4e6d730c44e57f5cf7
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-17 18:43:47 +0200

    list transformation and reusable

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,26 @@
 import List from './List.jsx'
 
 function App(){
+      const fruits = [
+        { id: 1, name: "Orange", calories: 95 },
+        { id: 2, name: "Apple", calories: 120 },
+        { id: 3, name: "Banana", calories: 45 },
+        { id: 4, name: "Coconut", calories: 75 },
+        { id: 5, name: "Pineapple", calories: 125 }
+    ];
+      const vegetables = [
+        { id: 6, name: "tomatoes", calories: 87 },
+        { id: 7, name: "beans", calories: 130 },
+        { id: 8, name: "maize", calories: 25 },
+        { id: 9, name: "Carrot", calories: 75 },
+        { id: 10, name: "corn", calories: 225 }
+    ];
     return(
-        <List/>
+       <>
+        <List items={fruits} category='fruits'/>
+        <List items={vegetables} category='vegetables'/>
+       
+       </>
         
     )
    
--- src/List.jsx
+++ src/List.jsx
@@ -1,27 +1,18 @@
 
-function List() {
-    const fruits = [
-        { id: 1, name: "Orange", calories: 95 },
-        { id: 2, name: "Apple", calories: 120 },
-        { id: 3, name: "Banana", calories: 45 },
-        { id: 4, name: "Coconut", calories: 75 },
-        { id: 5, name: "Pineapple", calories: 125 }
-    ];
+function List(props) {
+    const category=props.category;
+ const itemlist=props.items
+  
+  const listitems=itemlist.map(item=><li key={item.id}>
+                             {item.name}:&nbsp;
+                             <b>{item.calories}</b></li>)
+return(<>
+  <h3>{category}</h3>
 
-    const highcalfruits = fruits.filter(
-        fruit => fruit.calories >= 100
-    );
+<ol>{listitems}</ol>
+</>)
 
-    const listitems = highcalfruits.map(
-        highcalfruit => (
-            <li key={highcalfruit.id}>
-                {highcalfruit.name}:&nbsp;
-                <b>{highcalfruit.calories}</b>
-            </li>
-        )
-    );
 
-    return <ol>{listitems}</ol>;
 }
 
 export default List;

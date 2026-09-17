5f65e62c1fc1412a793a9839c4c81f6c5e51bbc7
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-17 18:22:41 +0200

    Using list rendering with filter to select wanted values in object

--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,27 @@
 
+function List() {
+    const fruits = [
+        { id: 1, name: "Orange", calories: 95 },
+        { id: 2, name: "Apple", calories: 120 },
+        { id: 3, name: "Banana", calories: 45 },
+        { id: 4, name: "Coconut", calories: 75 },
+        { id: 5, name: "Pineapple", calories: 125 }
+    ];
 
-function List(){
-const fruits=[{id:1 ,name:"Orange",calories:95}
-             ,{id:2,name:"Apple",calories:120}
-             ,{id:3,name:'Banana',calories:45},
-             {id:4,name:'Coconut',calories:75}
-             ,{id:5,name:'pineapple',calories:125}]
-// fruits.sort((a,b)=>a.name.localeCompare(b.name));
-//reverse alphabetical order
-//fruits.sort((a,b)=>b.name.localeCompare(a.name));
-//sort by calories in numerical order
-fruits.sort((a,b)=>a.calories-b.calories)
-const listiteams=fruits.map(fruit=><li key={fruit.id}>
-                             {fruit.name}:&nbsp;
-                             <b>{fruit.calories}</b></li>)
-return(<ol>{listiteams}</ol>)
+    const highcalfruits = fruits.filter(
+        fruit => fruit.calories >= 100
+    );
 
+    const listitems = highcalfruits.map(
+        highcalfruit => (
+            <li key={highcalfruit.id}>
+                {highcalfruit.name}:&nbsp;
+                <b>{highcalfruit.calories}</b>
+            </li>
+        )
+    );
 
+    return <ol>{listitems}</ol>;
 }
-export default List
+
+export default List;

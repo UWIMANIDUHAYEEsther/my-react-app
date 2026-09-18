a1763b5523b1a6b77e2c722cbab5441ece082304
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-18 07:43:25 +0200

    Usestate () hook and increment ,boolean

--- src/Mycomponent.jsx
+++ src/Mycomponent.jsx
@@ -3,13 +3,26 @@
 
 function Mycomponent(){
 
-let  [name,setname]=useState()
+const  [name,setname]=useState('Guest')
+const [age,setage]=useState(0)
+const[isEmployed,setisEmployed]=useState(false)
 const updateName=()=>{
     setname('Esther')
+}
+const incrementAge=()=>{
+    setage(age+1)
+}
+const toggleEmployedStatus=() =>{
+
+    setisEmployed(!isEmployed)
 }
 return (<div>
        <p>Name:{name}</p>
        <button onClick={updateName}>Set Name</button>
+        <p>Age:{age}</p>
+       <button onClick={incrementAge}>Increment Age</button>
+       <p>Is employed:{isEmployed ?'Yes':'No'}</p>
+       <button onClick={toggleEmployedStatus}>Toggle Status</button>
 </div>)
 
 }

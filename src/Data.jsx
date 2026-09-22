27abb2048ddf0101d98ee835618e33c96bb5ee4f
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-22 18:47:15 +0200

    use useEffect() to change color and title of web

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 function Data(){
     const [count, setCount] = useState(0)
+    const [color,setColor]=useState('pink')
          useEffect(()=>{
-            document.title=`Count:${count}`
-        },[count])
+            document.title=`Count:${count}${color}`
+          
+        },[count,color])
 
     function addCount(){
         setCount(c => c + 1)
@@ -12,12 +14,16 @@ function Data(){
      function subCount(){
         setCount(c => c - 1)
     }
+    function colorChange(){
+        setColor(c=>c==='pink'?'red':'pink')
+    }
 
     return(
         <>
-            <p>Count: {count}</p>
+            <p style={{color:color}}>Count: {count}</p>
             <button onClick={addCount}>Add</button>
             <button onClick={subCount}>Sub</button>
+            <button onClick={colorChange}>Change color</button>
         </>
     )
 }

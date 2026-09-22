daa4da033c8f3d7be55cec94ea5ecba1ef983935
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-22 19:13:07 +0200

    This code will use event useEffect to know width ,height of the window with the use of eventlistener

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react'
 
 function Data(){
-    const [count, setCount] = useState(0)
-    const [color,setColor]=useState('pink')
-         useEffect(()=>{
-            document.title=`Count:${count}${color}`
-          
-        },[count,color])
-
-    function addCount(){
-        setCount(c => c + 1)
-    }
-     function subCount(){
-        setCount(c => c - 1)
-    }
-    function colorChange(){
-        setColor(c=>c==='pink'?'red':'pink')
-    }
+  const[width,setWidth]=useState(window.innerWidth)
+  const[height,setHeight]=useState(window.innerHeight)
+  useEffect(()=>{
+    window.addEventListener('resize',handlerResize);
+  console.log('EVENT LISTENER ADDED')
+  return()=>{
+    window.removeEventListener('resize',handlerResize); 
+    console.log('EVENT LISTENER REMOVED')
+  }
+  },[])
+ function handlerResize(){
+  setWidth(window.innerWidth);
+  setHeight(window.innerHeight);
+ }
 
     return(
         <>
-            <p style={{color:color}}>Count: {count}</p>
-            <button onClick={addCount}>Add</button>
-            <button onClick={subCount}>Sub</button>
-            <button onClick={colorChange}>Change color</button>
+           <p>Window Width:{width}px</p> 
+            <p>Window Height:{height}px</p> 
         </>
     )
 }

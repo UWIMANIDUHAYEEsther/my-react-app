f7b22b6a5d6c1a1b18f52ec4917c9f8e93b4a0d8
UWIMANIDUHAYE <ug2635111@ines.ac.rw>
2026-09-23 18:38:40 +0200

    useRef in  react

--- src/Mycomp.jsx
+++ src/Mycomp.jsx
@@ -2,24 +2,52 @@
 import React, {useState,useEffect,useRef} from 'react'
 
 function Mycomp(){
-    const ref=useRef(0)
+    const inputref1=useRef(null)
+    const inputref2=useRef(null)
+    const inputref3=useRef(null)
 
     useEffect(()=>{
-        console.log('COMPONENT RENDERED')
+        console.log('COMPONENT RENDERED');
+        
+
     })
 
-    function handleClick(){
-   ref.current++;
-   console.log(ref.current)
+    function handleClick1(){
+    inputref1.current.focus();
+    inputref1.current.style.backgroundColor='yellow'
+     inputref2.current.style.backgroundColor=''
+      inputref3.current.style.backgroundColor=''
+    }
+    
+    function handleClick2(){
+    inputref2.current.focus();
+    inputref1.current.style.backgroundColor=''
+     inputref2.current.style.backgroundColor='yellow'
+      inputref3.current.style.backgroundColor=''
+    }
+    
+    function handleClick3(){
+    inputref3.current.focus();
+     inputref1.current.style.backgroundColor=''
+      inputref2.current.style.backgroundColor=''
+    inputref3.current.style.backgroundColor='yellow'
     }
 
 
     return(<div>
 
-        <button onClick={handleClick}>
-      Click Me !
+        <button onClick={handleClick1}>
+      Click Me1 !
     </button>
-    
+    <input ref={inputref1}/>
+       <button onClick={handleClick2}>
+      Click Me2 !
+    </button>
+    <input ref={inputref2}/>
+       <button onClick={handleClick3}>
+      Click Me3 !
+    </button>
+    <input ref={inputref3}/>
     </div>)
 
 
